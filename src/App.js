2ff0798f791d0146d3d82dda3317ec8f4ad8470b
Somesh Sunariwal <Someshsunariwal@gmail.com>
2021-07-26 13:08:47 +0530

    Okta Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter ,Route, useHistory, Switch } from 'react-router-dom';
+import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+import Home from './Component/Home/Home';
+import Login from './Component/Login/Login';
+import Protected from './Component/ProtectedPage/ProtectedPage';
+import { oktaAuthConfig, oktaSignInConfig } from './config';
 
-function App() {
+console.log(oktaAuthConfig.issuer);
+
+const oktaAuth = new OktaAuth(oktaAuthConfig);
+
+const App = () => {
+
+  const history = useHistory();
+
+  const customAuthHandler = () => {
+    history.push("/login")
+  };
+
+  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
+    history.replace(toRelativeUrl(originalUri, window.location.origin));
+  };
+
   return (
-    <div className="App">
-        <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Security
+        oktaAuth={oktaAuth}
+        onAuthRequired={customAuthHandler}
+        restoreOriginalUri={restoreOriginalUri}
+      >
+        <Switch>
+          <Route path='/' exact={true} component={Home} />
+          <SecureRoute path='/protected' component={Protected} />
+          <Route path='/login' render={() => <Login config={oktaSignInConfig} />} />
+          <Route path='/login/callback' component={LoginCallback} />
+        </Switch>
+      </Security>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,13 @@
+const oktaAuthConfig = {
+    issuer: process.env.REACT_APP_ISSUER,
+    clientId: process.env.REACT_APP_CLIENT_ID,
+    redirectUri: window.location.origin + '/login/callback',
+};
+
+const oktaSignInConfig = {
+    baseUrl: process.env.REACT_APP_BASE_URL,
+    clientId: process.env.REACT_APP_CLIENT_ID,
+    redirectUri: window.location.origin + '/login/callback',
+};
+
+export { oktaAuthConfig, oktaSignInConfig };
