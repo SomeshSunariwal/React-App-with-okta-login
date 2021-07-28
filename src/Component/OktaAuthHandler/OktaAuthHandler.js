505abf49d220b4786020c6a39bd6e2044834b3a2
Somesh Sunariwal <Someshsunariwal@gmail.com>
2021-07-28 16:51:58 +0530

    Code Commented

--- src/Component/OktaAuthHandler/OktaAuthHandler.js
+++ src/Component/OktaAuthHandler/OktaAuthHandler.js
@@ -28,11 +28,16 @@ const OktaAuthHandler = () => {
     >
       <Switch>
         <Route path="/" exact={true} component={Home} />
+        {/* Every Secure Route is Secured with okta. It will be not accessable till login*/}
         <SecureRoute path="/protected" component={Protected} />
+
+        {/* Normal Login Path */}
         <Route
           path="/login"
           render={() => <Login config={oktaSignInConfig} />}
         />
+
+        {/* CallBack url which is same as set in okta application (sign in Redirect URI) */}
         <Route path="/login/callback" component={LoginCallback} />
       </Switch>
     </Security>
